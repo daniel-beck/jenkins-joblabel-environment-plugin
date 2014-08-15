960e8023384f7bdc3ab7b188401095e496e03aca
Daniel Beck <daniel-beck@github.com>
2014-08-15 20:50:43 +0200

    Minor formatting

--- src/main/java/org/jenkinsci/plugins/joblabelenvironment/JobLabelEnvironmentContributor.java
+++ src/main/java/org/jenkinsci/plugins/joblabelenvironment/JobLabelEnvironmentContributor.java
@@ -11,8 +11,8 @@ import java.util.logging.Logger;
 
 @Extension
 public class JobLabelEnvironmentContributor extends EnvironmentContributor {
-	@Override
-	public void buildEnvironmentFor(Job job, EnvVars env, TaskListener listener) {
+    @Override
+    public void buildEnvironmentFor(Job job, EnvVars env, TaskListener listener) {
         if (!(job instanceof AbstractProject)) {
             LOGGER.finer(String.format("Skipping JOB_LABELS for %s because it's the wrong type", job.getFullDisplayName()));
             return;
@@ -25,7 +25,7 @@ public class JobLabelEnvironmentContributor extends EnvironmentContributor {
         }
         env.put("JOB_LABELS", labelString);
         LOGGER.fine(String.format("JOB_LABELS for %s are %s", project.getFullDisplayName(), labelString));
-	}
+    }
 
     private static final Logger LOGGER = Logger.getLogger(JobLabelEnvironmentContributor.class.getName());
 }
